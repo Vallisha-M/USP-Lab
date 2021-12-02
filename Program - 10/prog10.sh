c53dc6cff9478b67cca0a7dcff25c06cd5bf79d0
Vallisha-M <63452945+Vallisha-M@users.noreply.github.com>
2021-12-02 10:10:56 +0530

    Used for loop

--- Program - 10/prog10.sh
+++ Program - 10/prog10.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 i=1
 echo "Combinations of 123;"
-while [ $i -le 3 ]; do
-	j=1
-	while [ $j -le 3 ]; do
-		k=1
-		while [ $k -le 3 ]; do
+for i in 1 2 3; do
+	for j in 1 2 3; do
+		for k in 1 2 3; do
 			echo $i $j $k
-			k=`expr $k + 1`
 		done
-		j=`expr $j + 1`
-	done 
-	i=`expr $i + 1`
+	done
 done
 echo "--END--"
